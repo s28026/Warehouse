db9bfcbeb76e014f97000fcf97a92ec52e0cae2b
kawre <kenterq@gmail.com>
2025-06-09 16:05:47 +0200

    fix circular

--- backend/src/main/java/edu/pja/mas/warehouse/service/EmployeeService.java
+++ backend/src/main/java/edu/pja/mas/warehouse/service/EmployeeService.java
@@ -33,7 +33,6 @@ public class EmployeeService {
 
     private final DeliveryDriverService deliveryDriverService;
     private final @Lazy WarehouseEmployeeService warehouseEmployeeService;
-    private final EmployeeComplaintService employeeComplaintService;
 
     public Employee findById(String pesel) {
         return employeeRepository.findById(pesel)
